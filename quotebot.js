6166bddce792f1a97266b8ba361845d89e426acd
Freddie Sanchez <freddie.sanchez@gmail.com>
2016-07-11 21:15:47 -0700

    added time limits when it should speak.

--- quotebot.js
+++ quotebot.js
@@ -27,9 +27,26 @@ var replies =
     { encoding: 'utf-8' }
   ).split('\n').filter(function (x) { return x.trim().length > 0; });
 
+var should_speak = function(startHour, endHour, startDay, endDay) {
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  return day >= startDay && day <= endDay &&
+         hour>= startHour && hour <= endHour;
+};
+
 var quote = function () {
   var index = Math.floor(Math.random() * replies.length);
-  client.say(process.env.IRC_CHANNEL, replies[index]);
+
+  var startHour = 8;
+  var endHour = 18;
+  var startDay = 1; 
+  var endDay = 5;
+  var shouldSay = should_speak(startHour,endHour, startDay, endDay);
+
+  if (shouldSay) {
+    client.say(process.env.IRC_CHANNEL, replies[index]);
+  }
 };
 
 setInterval(quote, parseInt(process.env.QUOTE_INTERVAL));
